3fe5d640e929f19e77724558aabbbf9fae2d103b
Viking Falk <vikingfalk@gmail.com>
2021-04-13 11:15:27 +0200

    new layout working

--- app.js
+++ app.js
@@ -1,48 +1,13 @@
 const express = require('express');
-const axios = require('axios');
 require('dotenv').config();
-const COUNTRIES = require('./countries');
+const { selectRandomCountries, fetchPictureURL, mapPictureURLs, getFlagsForPictures } = require('./helpers')
 
 const app = express();
 const port = process.env.PORT || 8080;
-const API_KEY = process.env.API_KEY;
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-const getRandomInt = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-const selectRandomCountries = (prevIndex) => {
-  let countriesIndex = prevIndex || [];
-  const randomindex = getRandomInt(0, COUNTRIES.length - 1);
-  if (!countriesIndex.includes(randomindex)) {
-    countriesIndex.push(randomindex);
-  }
-  if (countriesIndex.length >= 6) {
-    return COUNTRIES.filter((country, index) => countriesIndex.includes(index));
-  }
-  return selectRandomCountries(countriesIndex);
-}
-
-const fetchPictureURL = async country => {
-  if (process.env.NODE_ENV === 'development') {
-    return 'https://picsum.photos/300/200';
-  }
-  const axiosRes = await axios.get(`https://pixabay.com/api/?key=${API_KEY}&q=${country}%20landmark%20-art%20-black%20-white%20-gray%20-woman%20-man&image_type=photo&category=buildings&safesearch=true&orientation=horizontal`);
-  const pictureIndex = getRandomInt(0, 5);
-  return axiosRes.data.hits[pictureIndex].largeImageURL ;
-}
-
-const mapPictureURLs = (countriesArray, pictureURLs) => {
-  return countriesArray.map((country, index) => ({
-    ...country,
-    pictureURL: pictureURLs[index]
-  }))
-}
 
 app.get('/', async (req, res) => {
   if (process.env.NODE_ENV === 'development') {
@@ -53,9 +18,12 @@ app.get('/', async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://vikingfalk.github.io');
   }
   try {
-    const randomCountries = selectRandomCountries();
+    const randomCountries = selectRandomCountries(6);
     const pictureURLs = await Promise.all(randomCountries.map(country => fetchPictureURL(country.name)));
-    const countriesData = mapPictureURLs(randomCountries, pictureURLs);
+    const countriesData = {
+      pictures: mapPictureURLs(randomCountries, pictureURLs),
+      flags: getFlagsForPictures(randomCountries)
+    };
     res.json(countriesData);
   } catch (err) {
     console.log(err.message);
@@ -64,5 +32,5 @@ app.get('/', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`QT server listening at http://localhost:${port}`);
 });
---
+++ helpers.js
@@ -0,0 +1,50 @@
+const axios = require('axios');
+const COUNTRIES = require('./countries');
+const API_KEY = process.env.API_KEY;
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const selectRandomCountries = (amount, prevIndexes) => {
+  let countryIndexes = prevIndexes || [];
+  const randomindex = getRandomInt(0, COUNTRIES.length - 1);
+  if (!countryIndexes.includes(randomindex)) {
+    countryIndexes.push(randomindex);
+  }
+  if (countryIndexes.length >= amount) {
+    return countryIndexes.map(countryIndex => COUNTRIES[countryIndex]);
+  }
+  return selectRandomCountries(amount, countryIndexes);
+}
+
+const fetchPictureURL = async country => {
+  if (process.env.NODE_ENV === 'development') {
+    return 'https://picsum.photos/600/400';
+  }
+  const axiosRes = await axios.get(`https://pixabay.com/api/?key=${API_KEY}&q=${country}%20landmark%20-art%20-black%20-white%20-gray%20-woman%20-man&image_type=photo&category=buildings&safesearch=true&orientation=horizontal`);
+  const pictureIndex = getRandomInt(0, 5);
+  return axiosRes.data.hits[pictureIndex].largeImageURL ;
+}
+
+const mapPictureURLs = (countriesArray, pictureURLs) => {
+  return countriesArray.map((country, index) => ({
+    ...country,
+    pictureURL: pictureURLs[index]
+  }))
+}
+
+const getFlagsForPictures = (pictureCountries) => {
+  return pictureCountries
+    .map(country => selectRandomCountries(6, [COUNTRIES.map(c => c.name).indexOf(country.name)]));
+}
+
+module.exports = {
+  getRandomInt,
+  selectRandomCountries,
+  fetchPictureURL,
+  mapPictureURLs,
+  getFlagsForPictures
+}
